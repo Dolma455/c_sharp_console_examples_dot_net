b3819f009067ffff1e6a2bf2176f15f7071cf5c2
Dolma <dolma1918@gmail.com>
2023-12-07 07:51:31 +0545

    feat:Multilevel inheritance created

--- obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
+++ obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b3337bcc4ca7b03f9b5d678b653aa8fb390b6ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09c103de8ba181ab5c1916805dcab81b79e1b922")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
