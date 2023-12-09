3fa927954115cdc0bb42571a24d495cb3ef6b2ef
Dolma <dolma1918@gmail.com>
2023-12-09 21:05:37 +0545

    feat: constructor created successfully

---
+++ constructor_example.cs
@@ -0,0 +1,13 @@
+using System;  
+   public class Employee  
+    {  
+        public Employee()  
+        {  
+            Console.WriteLine("Default Constructor Invoked");  
+        }  
+        public static void Main(string[] args)  
+        {  
+            Employee e1 = new Employee();  
+            Employee e2 = new Employee();  
+        }  
+    }  
--- multilevel_inheritance.cs
+++ multilevel_inheritance.cs
@@ -1,27 +1,27 @@
-using System;
-namespace multilevelinheritance {
-    public class Animal {
-        public void Eat() {
-            Console.WriteLine("Eating...");
-        }
-    }
-    public class Dog: Animal {
-        public void Bark() {
-            Console.WriteLine("Barking...");
-        }
-    }
-    public class Puppy: Dog {
-        public void Weep() {
-            Console.WriteLine("Weeping...");
-        }
-    }
-    class Program {
-         static void Main() {
-            Puppy hero=new Puppy();
-            hero.Bark();
-            hero.Eat();
-            hero.Weep();
+// using System;
+// namespace multilevelinheritance {
+//     public class Animal {
+//         public void Eat() {
+//             Console.WriteLine("Eating...");
+//         }
+//     }
+//     public class Dog: Animal {
+//         public void Bark() {
+//             Console.WriteLine("Barking...");
+//         }
+//     }
+//     public class Puppy: Dog {
+//         public void Weep() {
+//             Console.WriteLine("Weeping..."); 
+//         }
+//     }
+//     class Program {
+//          static void Main() {
+//             Puppy hero=new Puppy();
+//             hero.Bark();
+//             hero.Eat();
+//             hero.Weep();
 
-        }
-    }
-}
+//         }
+//     }
+// }
