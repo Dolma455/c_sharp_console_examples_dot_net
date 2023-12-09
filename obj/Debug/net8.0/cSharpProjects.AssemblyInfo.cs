f2928978f5eee0a9d2f301cad712507b3222bde7
Dolma <dolma1918@gmail.com>
2023-12-09 21:11:22 +0545

    feat: aggregation  example

--- obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
+++ obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+928c5bd67b78601f5b2bba1413c64a60a5c23fa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
