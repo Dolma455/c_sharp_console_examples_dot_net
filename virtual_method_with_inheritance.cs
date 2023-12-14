a16e860fb493c8ea31d219632a53f05c121e584f
Dolma <dolma1918@gmail.com>
2023-12-14 20:42:18 +0545

    feat: Virtual method with an inheritance

---
+++ virtual_method_with_inheritance.cs
@@ -0,0 +1,25 @@
+using System;
+namespace VirtualClass {
+    class Vir {
+        public virtual void message() {
+            Console.WriteLine("This is test");
+        }
+    }
+
+    class VirtualOne: Vir {
+        public override void message() {
+            Console.WriteLine("This is test one");
+
+
+        }
+
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            VirtualOne obj=new VirtualOne();
+            obj.message();
+            Console.ReadKey();
+        }
+    }
+}
