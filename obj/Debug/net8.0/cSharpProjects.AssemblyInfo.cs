569b5500adfaaab423417a934dc9552ac177aec4
Dolma <dolma1918@gmail.com>
2023-12-16 21:00:06 +0545

    feat: namespace with using keyword

--- obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
+++ obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f46d25a158ba77f220296abfc5e51aeb03ae384")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6df1cfbc165816a1704abf55a0dcda9396892dcb")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
