531b1f4776c0a95788aca85f2163ce951478e09a
Dolma <dolma1918@gmail.com>
2023-12-21 07:31:54 +0545

    feat: Dictionaries created

---
+++ dictionaries_example.cs
@@ -0,0 +1,15 @@
+using System;
+class Program {
+    static void Main() {
+
+        //Creating a dictionary with string keys and int] values
+        Dictionary<string, dynamic> ages=new Dictionary<string, dynamic>();
+        //Adding key/value pairs
+        ages["Alice"]=30;
+        ages["Bob"]=35;
+        ages["Charlie"]=20;
+
+        //Accessing values by key
+        Console.WriteLine(ages["Bob"]); 
+    }
+}
