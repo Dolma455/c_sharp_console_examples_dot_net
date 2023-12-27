dcb83dc556b5745695cc7e30ec75a57bffa227aa
Dolma <dolma1918@gmail.com>
2023-12-27 07:28:38 +0545

    feat: Queue example done

--- obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
+++ obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74ee31b6ec58292c8f2aaf37f5a34aa2f89bc3e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1adcd171a8de599cfafddfb09335b0a55fcc381c")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
