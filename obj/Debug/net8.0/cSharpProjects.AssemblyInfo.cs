565befc3ebcda42e02a60c597bd306f4c9aab64a
Dolma <dolma1918@gmail.com>
2023-12-27 07:42:11 +0545

    feat:stack examples done

--- obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
+++ obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcb83dc556b5745695cc7e30ec75a57bffa227aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dd356becf7af762b65896d8934aad2b9ebdcc5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
