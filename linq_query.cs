964ee38e52cb5093184f53ce58b11dfd7dc8d968
Dolma <dolma1918@gmail.com>
2024-01-04 07:48:23 +0545

    feat: LINQ query with lambda expression

---
+++ linq_query.cs
@@ -0,0 +1,13 @@
+using System.Globalization;
+
+class Program {
+    static void Main() {
+        List<int> numbers=new List<int>{1,2,3,4,5,6};
+        //a lambda expression in a LINQ query to filter even numbers
+        var evenNumbers=numbers.Where(num=>num%2==0);
+        //Output 
+        foreach(var num in evenNumbers){
+            Console.WriteLine(num);
+        }
+    }
+}
--- obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
+++ obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f3027062f1bbb0818a12f6f272f8319927e2c2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46d0a6fb4448ceaeecc60a27bfd0abd96b9c4a54")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
