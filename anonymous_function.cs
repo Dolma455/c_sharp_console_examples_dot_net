bebba7ee53323147e696aeab40ed6fe6d3372ae0
Dolma <dolma1918@gmail.com>
2024-01-04 07:37:44 +0545

    feat:Lambda Expression example

--- anonymous_function.cs
+++ anonymous_function.cs
@@ -1,15 +1,15 @@
-class Program {
+// class Program {
 
-    static void Main() {
-        Func<string,string,string> anonFunc=(string1,string2)=>{
-        string concatString=string1+string2;
-        return concatString;
-    };
+//     static void Main() {
+//         Func<string,string,string> anonFunc=(string1,string2)=>{
+//         string concatString=string1+string2;
+//         return concatString;
+//     };
 
-    //This code calls the anonymous function
-    string resultString=anonFunc("Hello","World");
-    Console.WriteLine(resultString);
+//     //This code calls the anonymous function
+//     string resultString=anonFunc("Hello","World");
+//     Console.WriteLine(resultString);
 
-    }
+//     }
     
-}
+// }
