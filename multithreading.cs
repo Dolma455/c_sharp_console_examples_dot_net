1b57d4f4a852175cf5accc8386680938da8c153a
Dolma <dolma1918@gmail.com>
2024-01-06 18:50:12 +0545

    feat: Multithreading Example done

---
+++ multithreading.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Threading;
+
+class Program {
+    static void Main() {
+        Thread thread1=new Thread(()=>PrintMessage("Hello from thread 1"));
+        thread1.Start();
+
+        Thread thread2=new Thread(()=>PrintMessage("Hello from thread2"));
+    }
+
+    static void PrintMessage(string message) {
+        for (int i=0;i<=5;i++) {
+                Console.WriteLine(message);
+                Thread.Sleep(1000);
+        }
+    }
+}
--- obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
+++ obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46d0a6fb4448ceaeecc60a27bfd0abd96b9c4a54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
