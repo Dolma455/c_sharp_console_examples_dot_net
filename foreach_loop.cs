c16644aae4f1683a5b6908c0d01e18f4048413ae
Dolma <dolma1918@gmail.com>
2024-01-07 22:01:58 +0545

    feat: foreach loop

---
+++ foreach_loop.cs
@@ -0,0 +1,11 @@
+class ForEach {
+    static void Main(string[] args) {
+        int []=args=[10,20,30,40,50];
+        foreach (int val in a) {
+            
+            Console.WriteLine(val);
+
+        }
+        Console.ReadKey();
+    }
+}
--- synchronization.cs
+++ synchronization.cs
@@ -1,33 +1,33 @@
-using System;
-using System.Threading;
+// using System;
+// using System.Threading;
 
-class Program {
-    static int counter=0;
-    static object lockObject=new object();
+// class Program {
+//     static int counter=0;
+//     static object lockObject=new object();
 
-    static void Main() {
-        Thread thread1=new Thread(()=>{
-            for(int i=0;i<5;i++) {
-                IncrementCounter("Hello from threead1");
+//     static void Main() {
+//         Thread thread1=new Thread(()=>{
+//             for(int i=0;i<5;i++) {
+//                 IncrementCounter("Hello from threead1");
             
-            }
-        });
-        thread1.Start();
+//             }
+//         });
+//         thread1.Start();
 
-        Thread thread2=new Thread(()=>{
-            for(int i=0;i<5;i++) {
-                IncrementCounter("Hello from thread2");
-            }
-        });
-        thread2.Start();
-    }
+//         Thread thread2=new Thread(()=>{
+//             for(int i=0;i<5;i++) {
+//                 IncrementCounter("Hello from thread2");
+//             }
+//         });
+//         thread2.Start();
+//     }
 
-    static void IncrementCounter(string message) {
-        lock(lockObject) {
-            counter++;
-            Console.WriteLine($"{message}.Counter:{counter}");
-            Thread.Sleep(1000);
-        }
-    }
+//     static void IncrementCounter(string message) {
+//         lock(lockObject) {
+//             counter++;
+//             Console.WriteLine($"{message}.Counter:{counter}");
+//             Thread.Sleep(1000);
+//         }
+//     }
 
-}
+// }
