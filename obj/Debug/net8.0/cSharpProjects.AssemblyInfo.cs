30bb9e42e99e231433564b90c1f235a13ecd84f5
Dolma <dolma1918@gmail.com>
2024-01-07 22:06:36 +0545

    feat: for loop

--- obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
+++ obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c16644aae4f1683a5b6908c0d01e18f4048413ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5bde26909639d6b8eda145a7dcf96adfa335d4c")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
