3079813dc057d201a9da3afe5caadb0b06d45c4a
Dolma <dolma1918@gmail.com>
2024-01-09 08:29:03 +0545

    Standard Query Operator Extension Methods

--- for_loop.cs
+++ for_loop.cs
@@ -1,7 +1,7 @@
-class Program {
-    static void Main() {
-        for (var i=0;i<=5;i++) {
-            Console.WriteLine(i);
-        }
-    }
-}
+// class Program {
+//     static void Main() {
+//         for (var i=0;i<=5;i++) {
+//             Console.WriteLine(i);
+//         }
+//     }
+// }
--- obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
+++ obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5bde26909639d6b8eda145a7dcf96adfa335d4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
