73776e3126004757a168c551ea6abdedce74f2a5
Dolma <dolma1918@gmail.com>
2024-01-16 23:20:51 +0545

    feat: Withouht syncronization example

---
+++ class_work.cs
@@ -0,0 +1,1 @@
+//WAp to select employees whose salary is greater than 20000 and whose address is Kathmandu using
--- obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
+++ obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea06adcf8287245aa2e28eb13c61aff0c78de711")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93fa5e2503dc35cfb1360958be5886ca468c4808")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
