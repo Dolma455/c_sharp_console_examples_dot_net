c7672b1f5a35c1ff2f19cb44a191177b85756461
Dolma <dolma1918@gmail.com>
2024-01-26 17:27:48 +0545

    feat: LINQ basics example

--- obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
+++ obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+770278414d4323182676b11d6c0fe2cf5ec02ed0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f8545745a02a20eb5c2be33c674123ba3311bae")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
