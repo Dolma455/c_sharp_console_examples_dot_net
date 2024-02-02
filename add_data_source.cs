c355f822310f54adde9531f857b780cbee6d1e75
Dolma <dolma1918@gmail.com>
2024-02-02 17:39:34 +0545

    feat: Adding data source

---
+++ add_data_source.cs
@@ -0,0 +1,51 @@
+public class Student
+{
+public string First {
+    get;
+    set;
+}
+public string Last {
+    get;
+    set;
+}
+public int ID {
+    get;
+    set;
+}
+
+public List<int> scores;
+
+}
+
+public class Program {
+   
+    static List<Student> students=[
+
+    new Student {First="Sita",Last="Patel",ID=11,scores=new List<int>{45,56,78,99}},
+    new Student {First="Rita",Last="Giri",ID=12, scores=new List<int>{45,67,87,65}},
+    new Student {First="Ram",Last="Giri",ID=13,scores=new List<int>{67,89,67,54}},
+    new Student {First="Hari",Last="Giri",ID=14,scores=new List<int>{67,89,67,54}},
+    new Student {First="Shyam",Last="Giri",ID=15,scores=new List<int>{67,89,67,54}},
+    new Student {First="Ganesh",Last="Giri",ID=16,scores=new List<int>{67,89,67,54}},
+
+];
+ static void Main() {
+
+
+IEnumerable<Student> studentQuery=from student in students
+where student.scores[0]>90
+select student;
+
+
+foreach (Student student in students) {
+    Console.WriteLine("{0},{1}",student.First,student.Last);
+}
+
+    }
+}
+
+
+
+
+
+
--- obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
+++ obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e2c3d364a01bdfb78c55262b639f9218560655f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e30bef02afe01952746f071ce997ff27c9346d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
