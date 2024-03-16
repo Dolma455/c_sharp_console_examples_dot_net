bf517b0f1f07a5a5412df76425cac51d410a30bb
Dolma <dolma1918@gmail.com>
2024-03-16 11:27:04 +0545

    feat: CRUD operation performed successfully

--- aggregate_function_example.cs
+++ aggregate_function_example.cs
@@ -1,47 +1,47 @@
-class Student {
-    public int id {get;set;}
-    public string name {get;set;}
-    public string address {get;set;}
-    public Student(int id, string name, string address) {
-        this.id=id;
-        this.name=name;
-        this.address=address;
-    }
+// class Student {
+//     public int id {get;set;}
+//     public string name {get;set;}
+//     public string address {get;set;}
+//     public Student(int id, string name, string address) {
+//         this.id=id;
+//         this.name=name;
+//         this.address=address;
+//     }
 
-}
+// }
 
-public class LinqTest {
-    public static void Main() {
-        List<Student> student=new List<Student>() {
-            new Student(1,"Hari","Kathmandu"),
-            new Student(2,"Shyam","Lalitpur"),
-            new Student(3,"Sita","Bhaktapur"),
-            new Student(4,"Nabin","Kathmandu"),
-        };
-        int maxId=student.Max(s=>s.id);
-        Console.WriteLine("Maximum id is: "+maxId);
-        int count=student.Count();
-        Console.WriteLine("Total number of students: "+count);
+// public class LinqTest {
+//     public static void Main() {
+//         List<Student> student=new List<Student>() {
+//             new Student(1,"Hari","Kathmandu"),
+//             new Student(2,"Shyam","Lalitpur"),
+//             new Student(3,"Sita","Bhaktapur"),
+//             new Student(4,"Nabin","Kathmandu"),
+//         };
+//         int maxId=student.Max(s=>s.id);
+//         Console.WriteLine("Maximum id is: "+maxId);
+//         int count=student.Count();
+//         Console.WriteLine("Total number of students: "+count);
 
-       var result=student.Where(s=>s.address.Equals("Kathmandu")).OrderBy(s=>s.name);
-       Console.WriteLine("id\t name\t address");
-       foreach (var a in result)
-       {
-           Console.WriteLine(a.id + "\t" + a.name + "\t" + a.address);
-       }
+//        var result=student.Where(s=>s.address.Equals("Kathmandu")).OrderBy(s=>s.name);
+//        Console.WriteLine("id\t name\t address");
+//        foreach (var a in result)
+//        {
+//            Console.WriteLine(a.id + "\t" + a.name + "\t" + a.address);
+//        }
 
-       var result1=student.OrderBy(s=>s.name);
-       foreach(var res in result1) {
-        Console.WriteLine(res.id + "\t" + res.name + "\t" + res.address);
-       }
+//        var result1=student.OrderBy(s=>s.name);
+//        foreach(var res in result1) {
+//         Console.WriteLine(res.id + "\t" + res.name + "\t" + res.address);
+//        }
 
-       //Use of group By
-       var result2=student.GroupBy(s=>s.address);
-       foreach(var res in result2) {
-        Console.WriteLine(res.Key);
-        foreach(var r in res) {
-            Console.WriteLine(r.id + "\t" + r.name + "\t" + r.address);
-        }
-       }
-    }
-}
+//        //Use of group By
+//        var result2=student.GroupBy(s=>s.address);
+//        foreach(var res in result2) {
+//         Console.WriteLine(res.Key);
+//         foreach(var r in res) {
+//             Console.WriteLine(r.id + "\t" + r.name + "\t" + r.address);
+//         }
+//        }
+//     }
+// }
--- obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
+++ obj/Debug/net8.0/cSharpProjects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fb93ec121fa1bc307c4fe2fd482dd7a0d60a012")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a9a37414f9ce86cc19edf46f4ee0c9f48708054")]
 [assembly: System.Reflection.AssemblyProductAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cSharpProjects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
